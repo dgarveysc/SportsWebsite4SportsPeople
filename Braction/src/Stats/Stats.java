6013f89596118b6e2d2e0905de06e16acc04b31b
Ari Cohen <aricohen@usc.edu>
2020-04-26 14:48:05 -0700

    classpath and stats fixes

--- Braction/src/Stats/Stats.java
+++ Braction/src/Stats/Stats.java
@@ -18,6 +18,13 @@ import java.util.List;
 import java.util.Set;
 import java.util.Vector;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/Stats")
 public class Stats extends HttpServlet {
@@ -28,7 +35,7 @@ public class Stats extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String testID = request.getAttribute("testID");
+		String testID = (String) request.getAttribute("testID");
 		
 		Connection conn = null;
 		Statement st = null;
@@ -381,9 +388,6 @@ public class Stats extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	
-	
 	
 	
 }
